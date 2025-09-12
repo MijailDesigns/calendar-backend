13621a2ce06bf6372d552113af3459dce4f464c6
Mijail Pulgar <mijail86@gmail.com>
2025-09-12 16:47:40 -0500

    configurar cors

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 require('dotenv').config()
 const fs = require('fs')
 const { dbConnection } = require('./database/config')
+const cors = require('cors');
 
 
 const app = express();
 
 // Base de datos
 dbConnection();
+
+// CORS
+app.use(cors())
 
 // Directorio publico
 app.use( express.static('public') );
